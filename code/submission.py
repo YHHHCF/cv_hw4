1dd55b12eb95382401c9a220feefe0e81c1c29f1
YHHHCF <huangzhejinthu@hotmail.com>
2019-04-02 22:20:48 -0400

    first commit

---
+++ code/submission.py
@@ -0,0 +1,72 @@
+"""
+Homework4.
+Replace 'pass' by your implementation.
+"""
+
+# Insert your package here
+
+
+'''
+Q2.1: Eight Point Algorithm
+    Input:  pts1, Nx2 Matrix
+            pts2, Nx2 Matrix
+            M, a scalar parameter computed as max (imwidth, imheight)
+    Output: F, the fundamental matrix
+'''
+def eightpoint(pts1, pts2, M):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q2.2: Seven Point Algorithm
+    Input:  pts1, Nx2 Matrix
+            pts2, Nx2 Matrix
+            M, a scalar parameter computed as max (imwidth, imheight)
+    Output: Farray, a list of estimated fundamental matrix.
+'''
+def sevenpoint(pts1, pts2, M):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.1: Compute the essential matrix E.
+    Input:  F, fundamental matrix
+            K1, internal camera calibration matrix of camera 1
+            K2, internal camera calibration matrix of camera 2
+    Output: E, the essential matrix
+'''
+def essentialMatrix(F, K1, K2):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
+    Input:  C1, the 3x4 camera matrix
+            pts1, the Nx2 matrix with the 2D image coordinates per row
+            C2, the 3x4 camera matrix
+            pts2, the Nx2 matrix with the 2D image coordinates per row
+    Output: P, the Nx3 matrix with the corresponding 3D points per row
+            err, the reprojection error.
+'''
+def triangulate(C1, pts1, C2, pts2):
+    # Replace pass by your implementation
+    pass
+
+
+'''
+Q4.1: 3D visualization of the temple images.
+    Input:  im1, the first image
+            im2, the second image
+            F, the fundamental matrix
+            x1, x-coordinates of a pixel on im1
+            y1, y-coordinates of a pixel on im1
+    Output: x2, x-coordinates of the pixel on im2
+            y2, y-coordinates of the pixel on im2
+
+'''
+def epipolarCorrespondence(im1, im2, F, x1, y1):
+    # Replace pass by your implementation
+    pass
